2be1382e40ddd69ed2b39be4373e33e71a55bb2c
rezadyk <safri.110798@gmail.com>
2021-11-21 15:13:53 +0700

    add other solution

--- test4.js
+++ test4.js
@@ -5,16 +5,28 @@
  * Expected Result:
  * 8
  */
-const numbers = [9, 6, 4, 2, 3, 5, 7, 0, 1];
+const numbers = [9, 6, 4, 2, 3, 5, 7, 11];
 
 function result(numbers) {
   // Your Code Here
-  let sorted = numbers.sort();
-  for (let i = 0; i < sorted.length; i++) {
-    if (sorted[i] != i) {
-      return i;
+  // let sorted = numbers.sort();
+  // for (let i = 0; i < sorted.length; i++) {
+  //   if (sorted[i] != i) {
+  //     return i;
+  //   }
+  // }
+
+  // other solution, find multiple missing number start from smallest to largest number in a array
+  let result = [];
+  let sorted = numbers.sort((a, b) => {
+    return a - b;
+  });
+  for (let i = sorted[0]; i < sorted[sorted.length - 1]; i++) {
+    if (sorted.indexOf(i) == -1) {
+      result.push(i);
     }
   }
+  return result;
 }
 
 console.log(result(numbers));
