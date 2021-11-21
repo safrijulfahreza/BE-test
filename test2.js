af9f19663da3e0868a30445f9a56f1e1df1133b6
rezadyk <safri.110798@gmail.com>
2021-11-21 15:12:02 +0700

    add other solution

--- test2.js
+++ test2.js
@@ -5,7 +5,7 @@
  * Expected Result:
  * [1, 2, 3, 4, 5]
  */
-const data = [1, 4, 2, 3, 5, 3, 2, 4];
+const data = [1, 4, 2, 3, 5, 3, 2, 3, 4];
 
 function result(data) {
   // Your Code Here
@@ -17,6 +17,9 @@ function result(data) {
     }
   }
   return results;
+
+  // other solution
+  // return [...new Set(data)].sort();
 }
 
 console.log(result(data));
